07d0232485f71082c4bca465b9deea2d42fc4a03
naresho23 <151425787+naresho23@users.noreply.github.com>
2025-05-17 06:34:48 +0000

    initial version

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 import * as core from '@actions/core';
+import * as core from '@actions/github';
 
-const name = core.getInput('name');
+
+try {
+    const name = core.getInput('name');
 const output_value = `Hello ${name} from Naresh!`;
 core.setOutput('greeting', output_value);
+}
+ catch (error) {
+  core.setFailed(error.message);
+}
